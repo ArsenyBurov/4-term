e7475e067c3967bce2d77d2cffec6b10da8c2df9
Arseny <ars15burov@yandex.ru>
2026-02-19 11:15:00 +0400

    change encoding

--- Data-structures-and-algorithms/Code/Sortings/radix-and-counting.cpp
+++ Data-structures-and-algorithms/Code/Sortings/radix-and-counting.cpp
@@ -1,15 +1,15 @@
-#include <iostream>
+п»ї#include <iostream>
 
 using namespace std;
 
 void countingSort(int A[], int n, int k) {
-    //массив счетчиков размера k, заполняем 0
+    //РјР°СЃСЃРёРІ СЃС‡РµС‚С‡РёРєРѕРІ СЂР°Р·РјРµСЂР° k, Р·Р°РїРѕР»РЅСЏРµРј 0
     int* C = new int[k]();
-    //количество каждого числа
+    //РєРѕР»РёС‡РµСЃС‚РІРѕ РєР°Р¶РґРѕРіРѕ С‡РёСЃР»Р°
     for (int i = 0; i < n; i++) {
         C[A[i]]++;
     }
-    //восстанавливаем отсортированный массив
+    //РІРѕСЃСЃС‚Р°РЅР°РІР»РёРІР°РµРј РѕС‚СЃРѕСЂС‚РёСЂРѕРІР°РЅРЅС‹Р№ РјР°СЃСЃРёРІ
     int pos = 0;
     for (int number = 0; number < k; number++) {
         for (int i = 0; i < C[number]; i++) {
@@ -29,30 +29,30 @@ int digit(int x, int i) {
 
 
 void radixSortLSD(int A[], int N, int m, int P = 10) {
-    //создаем P дополнительных массивов
+    //СЃРѕР·РґР°РµРј P РґРѕРїРѕР»РЅРёС‚РµР»СЊРЅС‹С… РјР°СЃСЃРёРІРѕРІ
     int** dop = new int* [P];
     for (int i = 0; i < P; i++) {
         dop[i] = new int[N];
     }
 
-    //массив размеров дополнительных массивов
+    //РјР°СЃСЃРёРІ СЂР°Р·РјРµСЂРѕРІ РґРѕРїРѕР»РЅРёС‚РµР»СЊРЅС‹С… РјР°СЃСЃРёРІРѕРІ
     int* dopSizes = new int[P]();
 
-    //проходим по всем разрядам от 1 до m
+    //РїСЂРѕС…РѕРґРёРј РїРѕ РІСЃРµРј СЂР°Р·СЂСЏРґР°Рј РѕС‚ 1 РґРѕ m
     for (int i = 1; i <= m; i++) {
-        //очищаем доп массивы
+        //РѕС‡РёС‰Р°РµРј РґРѕРї РјР°СЃСЃРёРІС‹
         for (int b = 0; b < P; b++) {
             dopSizes[b] = 0;
         }
 
-        //распределяем элементы по доп массивам
+        //СЂР°СЃРїСЂРµРґРµР»СЏРµРј СЌР»РµРјРµРЅС‚С‹ РїРѕ РґРѕРї РјР°СЃСЃРёРІР°Рј
         for (int j = 0; j < N; j++) {
             int d = digit(A[j], i);
             dop[d][dopSizes[d]] = A[j];
             dopSizes[d]++;
         }
 
-        //перезаписываем массив A из доп массивов
+        //РїРµСЂРµР·Р°РїРёСЃС‹РІР°РµРј РјР°СЃСЃРёРІ A РёР· РґРѕРї РјР°СЃСЃРёРІРѕРІ
         int pos = 0;
         for (int b = 0; b < P; b++) {
             for (int j = 0; j < dopSizes[b]; j++) {
@@ -101,32 +101,32 @@ int findk(int arr[], int n) {
 
 int main() {
     setlocale(LC_ALL, "RUS");
-    cout << "сортировка подсчетом" << endl;
+    cout << "СЃРѕСЂС‚РёСЂРѕРІРєР° РїРѕРґСЃС‡РµС‚РѕРј" << endl;
     int arr[] = { 4, 2, 2, 8, 3, 3, 1, 0, 4, 0, 5, 7 };
     int n1 = sizeof(arr) / sizeof(arr[0]);
     int k1 = findk(arr, n1);
 
-    cout << "исходный массив: ";
+    cout << "РёСЃС…РѕРґРЅС‹Р№ РјР°СЃСЃРёРІ: ";
     printArr(arr, n1);
 
     countingSort(arr, n1, k1);
 
-    cout << "результат countingSort: ";
+    cout << "СЂРµР·СѓР»СЊС‚Р°С‚ countingSort: ";
     printArr(arr, n1);
     cout << endl;
 
-    cout << "поразрядная сортировка LSD" << endl;
+    cout << "РїРѕСЂР°Р·СЂСЏРґРЅР°СЏ СЃРѕСЂС‚РёСЂРѕРІРєР° LSD" << endl;
     int arr2[] = { 160, 60, 144, 12, 78, 56, 2, 122 };
     int n2 = sizeof(arr2) / sizeof(arr2[0]);
     int k2 = 10; 
     int m = findMaxRazryad(arr2, n2);
 
-    cout << "исходный массив: ";
+    cout << "РёСЃС…РѕРґРЅС‹Р№ РјР°СЃСЃРёРІ: ";
     printArr(arr2, n2);
 
     radixSortLSD(arr2, n2, m, k2);
 
-    cout << "результат radixSortLSD: ";
+    cout << "СЂРµР·СѓР»СЊС‚Р°С‚ radixSortLSD: ";
     printArr(arr2, n2);
 
     return 0;
